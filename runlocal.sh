8f648f8563eb69c2eb9262d5a0eb35be7bd31ee2
Rick O <rosowski@gmail.com>
2016-11-11 09:57:15 -0600

    Delete runlocal.sh
    Removing extraneous local build files in favor of orchestration tooling and scripting in main project.

--- runlocal.sh
+++
@@ -1,2 +0,0 @@
-echo "Starting turbine ........... streaming on port 8989"
-docker run --name netflix-turbine -p 8989:8989 -p 8990:8990 --env-file ~/ibm-cloud-architecture/refarch-cloudnative-container-utils/env/env-eureka-only-local -d netflix-turbine:latest
